bbda6057445896830b0d39830975719017e4468e
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2023-07-28 16:49:43 +0300

    Update bot-builder.js

--- src/assets/backend/bot-builder.js
+++ src/assets/backend/bot-builder.js
@@ -1,1 +1,12 @@
 console.log('Bot Builder Loaded');
+
+class BotBuilder  {
+  // calculate distance
+  function distance(point1, point2)
+  {
+      const dx = point1.x - point2.x;
+      const dy = point1.y - point2.y;
+    
+      return Math.sqrt(dx * dx + dy * dy);
+  }
+}
